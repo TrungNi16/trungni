9c1d0c7b5e3a79dff01b0ff1bc59bbdcd83b668b
TrungNi16 <phiquangtrung6@gmail.com>
2026-07-23 21:17:31 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,26 +1,61 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Cấu hình Firebase
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    updateDoc,
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    serverTimestamp,
+    query,
+    where
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCghsuyQOhK6EYM5tyMVeMyMORE-yy79UE",
-  authDomain: "trungni.firebaseapp.com",
-  projectId: "trungni",
-  storageBucket: "trungni.firebasestorage.app",
-  messagingSenderId: "195760563004",
-  appId: "1:195760563004:web:30b4c6a98eee4be1a23439"
+    apiKey: "AIzaSyCghsuyQOhK6EYM5tyMVeMyMORE-yy79UE",
+    authDomain: "trungni.firebaseapp.com",
+    projectId: "trungni",
+    storageBucket: "trungni.firebasestorage.app",
+    messagingSenderId: "195760563004",
+    appId: "1:195760563004:web:30b4c6a98eee4be1a23439"
 };
 
-// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 
-// Khởi tạo Authentication
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const provider = new GoogleAuthProvider();
 
-// Khởi tạo Firestore
-const db = getFirestore(app);
-
-// Export để các file khác sử dụng
-export { auth, db };
+export {
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged,
+    doc,
+    getDoc,
+    setDoc,
+    updateDoc,
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    serverTimestamp,
+    query,
+    where
+};
