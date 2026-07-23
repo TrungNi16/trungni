ec99ba20512fb118c44e6e77ba4455f1314ed16f
TrungNi16 <phiquangtrung6@gmail.com>
2026-07-23 21:30:05 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 
 import {
     getAuth,
     GoogleAuthProvider,
-    signInWithPopup,
     signOut,
-    onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    browserLocalPersistence,
+    setPersistence
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 import {
     getFirestore,
@@ -14,48 +18,99 @@ import {
     getDoc,
     setDoc,
     updateDoc,
+    deleteDoc,
     collection,
     addDoc,
     getDocs,
-    deleteDoc,
     serverTimestamp,
     query,
     where
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 import {
-    getStorage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyCghsuyQOhK6EYM5tyMVeMyMORE-yy79UE",
+
     authDomain: "trungni.firebaseapp.com",
+
     projectId: "trungni",
+
     storageBucket: "trungni.firebasestorage.app",
+
     messagingSenderId: "195760563004",
+
     appId: "1:195760563004:web:30b4c6a98eee4be1a23439"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+await setPersistence(auth, browserLocalPersistence);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+const provider = new GoogleAuthProvider();
 
 export {
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged,
-    doc,
-    getDoc,
-    setDoc,
-    updateDoc,
-    collection,
-    addDoc,
-    getDocs,
-    deleteDoc,
-    serverTimestamp,
-    query,
-    where
+
+auth,
+
+db,
+
+storage,
+
+provider,
+
+signOut,
+
+onAuthStateChanged,
+
+createUserWithEmailAndPassword,
+
+signInWithEmailAndPassword,
+
+signInWithPopup,
+
+doc,
+
+getDoc,
+
+setDoc,
+
+updateDoc,
+
+deleteDoc,
+
+collection,
+
+addDoc,
+
+getDocs,
+
+serverTimestamp,
+
+query,
+
+where,
+
+ref,
+
+uploadBytes,
+
+getDownloadURL,
+
+deleteObject
+
 };
