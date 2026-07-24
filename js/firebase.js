942777b7cce7b82a884d487776c37e5f8af78170
TrungNi16 <phiquangtrung6@gmail.com>
2026-07-24 13:42:05 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,89 +1,128 @@
+// ===============================
+// Firebase Config
+// ===============================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 
 import {
-  getAuth,
-  GoogleAuthProvider,
-  signOut,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  browserLocalPersistence,
-  setPersistence
+    getAuth,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    browserLocalPersistence,
+    setPersistence
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  deleteDoc,
-  collection,
-  addDoc,
-  getDocs,
-  serverTimestamp,
-  query,
-  where,
-  onSnapshot
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    updateDoc,
+    deleteDoc,
+    collection,
+    addDoc,
+    getDocs,
+    serverTimestamp,
+    query,
+    where
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 import {
-  getStorage,
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  deleteObject
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
 
+// ===============================
+// Firebase Config
+// ===============================
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCghsuyQOhK6EYM5tyMVeMyMORE-yy79UE",
-  authDomain: "trungni.firebaseapp.com",
-  databaseURL: "https://trungni-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "trungni",
-  storageBucket: "trungni.firebasestorage.app",
-  messagingSenderId: "195760563004",
-  appId: "1:195760563004:web:30b4c6a98eee4be1a23439"
+
+    apiKey: "AIzaSyCghsuyQOhK6EYM5tyMVeMyMORE-yy79UE",
+
+    authDomain: "trungni.firebaseapp.com",
+
+    projectId: "trungni",
+
+    storageBucket: "trungni.firebasestorage.app",
+
+    messagingSenderId: "195760563004",
+
+    appId: "1:195760563004:web:30b4c6a98eee4be1a23439"
+
 };
+
+// ===============================
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+
 await setPersistence(auth, browserLocalPersistence);
 
 const db = getFirestore(app);
+
 const storage = getStorage(app);
+
 const provider = new GoogleAuthProvider();
 
+// ===============================
+
 export {
-  auth,
-  db,
-  storage,
-  provider,
 
-  signOut,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
+auth,
 
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  deleteDoc,
+db,
 
-  collection,
-  addDoc,
-  getDocs,
+storage,
 
-  serverTimestamp,
-  query,
-  where,
-  onSnapshot,
+provider,
 
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  deleteObject
+signOut,
+
+onAuthStateChanged,
+
+createUserWithEmailAndPassword,
+
+signInWithEmailAndPassword,
+
+signInWithPopup,
+
+doc,
+
+getDoc,
+
+setDoc,
+
+updateDoc,
+
+deleteDoc,
+
+collection,
+
+addDoc,
+
+getDocs,
+
+serverTimestamp,
+
+query,
+
+where,
+
+ref,
+
+uploadBytes,
+
+getDownloadURL,
+
+deleteObject
+
 };
